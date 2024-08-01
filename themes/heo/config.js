d1615cfb1905cac40b83451360ca1b9fd4992ece
lengsukq <lengsukq@foxmail.com>
2024-08-01 11:18:57 +0800

    部分设置自定义2

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,8 +5,10 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    // { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客！', url: 'https://blog.lengsu.top' },
+    { title: '得失既成，不若坦然。', url: 'https://blog.lengsu.top' },
+    { title: '就算败了，也要一如既往。', url: 'https://blog.lengsu.top' },
+    { title: '当以向梦行，不复当年勇。', url: 'https://blog.lengsu.top' },
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -15,12 +17,10 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '编程成就思维',
-  HEO_HERO_TITLE_2: '思维决定编程',
-  HEO_HERO_TITLE_3: 'blog.lengsu.top',
-  HEO_HERO_TITLE_4: '新版上线',
-  // HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  // HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '编程铸就思维',
+  HEO_HERO_TITLE_2: '思维成就编程',
+  HEO_HERO_TITLE_3: '走在全栈路上',
+  HEO_HERO_TITLE_LINK: 'https://blog.lengsu.top',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
@@ -34,7 +34,7 @@ const CONFIG = {
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
-    '你好！我是',
+    '你好！我是leng',
     '🔍 分享与热心帮助',
     '🤝 专修交互与设计',
     '🏃 脚踏实地行动派',
@@ -46,10 +46,10 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  HEO_INFO_CARD_URL2: 'https://github.com/lengsukq/',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
-  HEO_INFO_CARD_TEXT3: '了解更多',
+  // HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
+  // HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
